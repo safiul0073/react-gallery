8d6b28cc473a6331689c668fd5d46480b145cd9d
safiul0073 <parsonal494@gmail.com>
2023-11-04 01:12:30 +0600

    project to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
+import { useState } from "react";
+import "./App.css";
+import { Gallery } from "./components/gallery";
+import { IMAGE_LIST } from "./data";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [imageList, setImageList] = useState(IMAGE_LIST);
+
+    const [draggedItem, setDraggedItem] = useState(null);
+
+    const handleMoveItem = (sourceIndex, targetIndex) => {
+        console.log("moving", sourceIndex, targetIndex);
+        // const updatedCards = [...imageList];
+        // const [movedCard] = updatedCards.splice(sourceIndex, 1);
+        // updatedCards.splice(targetIndex, 0, movedCard);
+        // setImageList(updatedCards);
+    };
+
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div className="w-100 mx-auto py-8 bg-cyan-50">
+                <Gallery />
+            </div>
+        </DndProvider>
+    );
 }
 
 export default App;
